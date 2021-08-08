ffe81b5eb00c2171d649078d02d6cb8036ba9248
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-08-08 19:58:05 +0600

    Student model created

---
+++ src/main/java/noor/tanvir/studentmanager/model/Student.java
@@ -0,0 +1,91 @@
+package noor.tanvir.studentmanager.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String department;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String studentCode;
+
+    public Student() {
+    }
+    public Student(Long id, String name, String email, String department, String imageUrl, String studentCode) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+        this.imageUrl = imageUrl;
+        this.studentCode = studentCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(String studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", department='" + department + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", studentCode='" + studentCode + '\'' +
+                '}';
+    }
+}
