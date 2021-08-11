c4c8b5e76907f7aba2219f9e3840cfa1d92c378b
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-08-12 03:10:42 +0600

    Student service and model created

--- Frontend/studentmanagerapp/src/app/student.service.ts
+++ Frontend/studentmanagerapp/src/app/student.service.ts
@@ -1,17 +1,24 @@
-import {root} from "rxjs/internal-compatibility";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Student} from "./student";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { Student } from './student';
 
-@injectable({
-    providedIn : root
-  })
+@Injectable({
+  providedIn: 'root'
+})
 export class StudentService {
   private apiServerUrl = '';
-
-  constructor(private http: HttpClient) {}
-
-  public getStudents(): Observable<Student[]> {
-    return this.http.get<Student[]>(`${this.apiServerUrl}/student/all`);
+  constructor(private http: HttpClient) { }
+  public getStudents(): Observable<Student []>{
+    return this.http.get<Student []>(`${this.apiServerUrl}/student/all`);
+  }
+  public addStudents(student: Student): Observable<Student>{
+    return this.http.post<Student>(`${this.apiServerUrl}/student/add`, student);
+  }
+  public updateStudents(student: Student): Observable<Student>{
+    return this.http.put<Student>(`${this.apiServerUrl}/student/update`, student);
+  }
+  public deleteStudents(studentId: number): Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/student/delete/${studentId}`);
   }
 }
